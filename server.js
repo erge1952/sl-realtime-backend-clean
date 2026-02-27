b323a95206c2f382cf5e45487a3d03fb3723473f
erge1952 <erki@hotmail.se>
2026-02-27 16:56:39 +0100

    Update server.js

--- server.js
+++ server.js
@@ -130,15 +130,16 @@ if (shapeId) {
     [shapeId]
   );
 }
-  const [shapeRows] = await db.query(
-    `
-    SELECT shape_pt_lat, shape_pt_lon
-    FROM shapes
-    WHERE shape_id = ?
-    ORDER BY shape_pt_sequence
-    `,
-    [shapeId]
-  );
+let shapeRows = [];
+[shapeRows] = await db.query(
+  `
+  SELECT shape_pt_lat, shape_pt_lon
+  FROM shapes
+  WHERE shape_id = ?
+  ORDER BY shape_pt_sequence
+  `,
+  [shapeId]
+);
 
   const data = {
     routeId: route.route_id,
